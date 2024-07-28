1debc7f0451e55ed3a6e3bdcc8e459012dcdc379
root <root@localhost>
2024-07-29 01:38:20 +0530

    auto publish 1722197300588

---
+++ src/main.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+
+int main()
+{
+    int x =5;
+    if (x>90) {
+    printf(" grade A");}
+    
+    else {
+    if(x>80){ 
+    printf("grade B");
+    else { 
+    if(x>50) 
+    printf ("grade C"); 
+    else { printf(" grade D");} 
+    }
+    
+    
+    }
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    return 0;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+    
+
+
+
+
+
+    
+    
+
+      
+      
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
